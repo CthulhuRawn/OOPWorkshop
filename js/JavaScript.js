8b204206f01b24f84282321e2a108eccd0f1e2c8
CthulhuRawn <galron995@gmail.com>
2016-08-12 19:12:56 +0300

    Added treatment page

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -8,3 +8,22 @@ $(document).ready(function(){
                                 $(this).tab('show');
                         })
                 })
+				
+addRow = function(tableName, button){
+	$(button).addClass('disabled');
+	$(button).attr("disabled", true);
+	var date = Date.now();
+	var newRow = '<tr><td><button class="btn btn-primary" onclick="saveRow(this,' + date + ','+ $(button).attr('id')+ ')">Save</button></td><td><input id="' + date + '" type="text" class="form-control"></input></td><td></td></tr>';
+	$('#'+tableName +' > tbody:last-child').append(newRow);
+}
+
+saveRow = function(saveButton,textBoxId,addButton){
+	$(addButton).removeClass('disabled');
+	$(addButton).attr("disabled", false);
+	var value = $('#' +textBoxId).val()
+	var parent = $('#' +textBoxId).parent();
+	$('#' +textBoxId).remove();
+	$(parent).append(value);
+	$(saveButton).remove();
+
+}
